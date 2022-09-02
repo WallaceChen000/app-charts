a236a2426c12902ebac87df7a94393aef8adcf12
Wallace Chen <wallacechen8671@gmail.com>
2022-09-02 09:43:16 +0800

    kubernetes charts

---
+++ app-charts-cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+rm -f index-cache.yaml
+sleep 1
+#/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./ > index-cache.yaml
+#/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./
+/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./ | tee /dev/stdout > index.yaml
+#sleep 1
+#mv index-cache.yaml index.yaml
