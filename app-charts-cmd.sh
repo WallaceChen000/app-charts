c7df33ef27838d989566b1a65b3b2efa03e3ec0a
Wallace Chen <wallace.chen@infortrend.com>
2022-09-15 11:10:42 +0800

    20220915

--- app-charts-cmd.sh
+++ app-charts-cmd.sh
@@ -5,6 +5,6 @@ rm -f index-cache.yaml
 sleep 1
 #/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./ > index-cache.yaml
 #/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./
-/usr/local/bin/chartmuseum --gen-index --storage local --storage-local-rootdir ./ | tee /dev/stdout > index.yaml
+${PWD}/chartmuseum --gen-index --storage local --storage-local-rootdir ./ | tee /dev/stdout > index.yaml
 #sleep 1
 #mv index-cache.yaml index.yaml
